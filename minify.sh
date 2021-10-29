77ad2d15b59f35445ccc14e6a9d5263919f13321
Philipp Kiemle <philipp.kiemle@gmail.com>
2021-10-29 18:53:31 +0200

    Use bash array syntax to loop through files

--- minify.sh
+++ minify.sh
@@ -17,9 +17,6 @@ fi
 PROJECT_ROOT=$(pwd)
 OUTPUT_DIR="$PROJECT_ROOT/build"
 
-FILES=""
-OUTPUT_FILES=""
-
 if [ ! -d "$OUTPUT_DIR" ]; then
 		mkdir $OUTPUT_DIR
 fi
@@ -34,16 +31,16 @@ if [ -d "$OUTPUT_DIR/png" ]; then
 fi
 mkdir $OUTPUT_DIR/png
 
-for i in $PROJECT_ROOT/src/*.svg; do
-		FILES="$FILES$i "
+for i in "$PROJECT_ROOT"/src/*.svg; do
+		FILES+=("$i")
 		SVG_OUTPUT=${i/src/build\/svg}
-		OUTPUT_FILES="$OUTPUT_FILES$SVG_OUTPUT "
+		OUTPUT_FILES+=("$SVG_OUTPUT")
 		PNG_OUTPUT=${i/src/build\/png}
 		PNG_OUTPUT=${PNG_OUTPUT/.svg/.png}
 		${SVGEXPORT} "$i" $PNG_OUTPUT 512
 done
 
-${SVGO} $FILES -o $OUTPUT_FILES
+${SVGO} "${FILES[@]}" -o "${OUTPUT_FILES[@]}"
 
 cd $PROJECT_ROOT/build/
 echo "Creating archive"
